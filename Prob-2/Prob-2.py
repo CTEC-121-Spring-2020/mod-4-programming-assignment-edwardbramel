5748c5009cfa979c898ccd5ffccd9a39dd14c418
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-14 21:20:46 -0700

    done did the seconf

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -6,18 +6,21 @@
 
 from graphics import *
 
+
 def main():
-     win = GraphWin()
-     shape = Circle(Point(50,50), 20)
-     shape.setOutline("red")
-     shape.setFill("red")
-     shape.draw(win)
-     for i in range(5):
-          p = win.getMouse()
-          c = shape.getCenter()
-          dx = p.getX() - c.getX()
-          dy = p.getY() - c.getY()
-          shape.move(dx,dy)
-     win.close()
+    win = GraphWin()
+    # added rectangle instead of circle and had to add another point on x,y for the rectangle
+    shape = Rectangle(Point(50, 50), Point(120, 120))
+    # made it a new color just cause also got rid of outline
+    shape.setFill("Blue")
+    shape.draw(win)
+    for i in range(5):
+        p = win.getMouse()
+        c = shape.getCenter()
+        dx = p.getX() - c.getX()
+        dy = p.getY() - c.getY()
+        shape.move(dx, dy)
+    win.close()
+
 
 main()
