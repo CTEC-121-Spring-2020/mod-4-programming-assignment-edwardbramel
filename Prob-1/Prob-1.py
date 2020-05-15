63d362ee2ec5a709eb04d007211506229c90ec09
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-14 21:00:04 -0700

    done with prob 1

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,7 +2,7 @@
 #   Programming Assignment 6
 #       Prob-1.py
 
-# <YOUR NAME>
+# Edward LaManna
 
 # INSTRUCTIONS:
 #   Insert a comment above each line of code in the program below to describe
@@ -11,18 +11,32 @@
 
 from graphics import *
 
+
 def main():
-     win = GraphWin()
-     shape = Circle(Point(50,50), 20)
-     shape.setOutline("red")
-     shape.setFill("red")
-     shape.draw(win)
-     for i in range(5):
-          p = win.getMouse()
-          c = shape.getCenter()
-          dx = p.getX() - c.getX()
-          dy = p.getY() - c.getY()
-          shape.move(dx,dy)
-     win.close()
+    # the win=graph makes a tabe open when you run the program and tells the computer hey were making a graph
+    win = GraphWin()
+    # the shape=circ lets the computer know that the circle should be at x,y on the graph with a radious of 20
+    shape = Circle(Point(50, 50), 20)
+    # .setoutline give the circle an outline of red
+    shape.setOutline("red")
+    # .setfill makes the inside of the circle red
+    shape.setFill("red")
+    # finally telling the computer to plot the previous varibles
+    shape.draw(win)
+    # giving i a range of 5 comming back to this i can click 5 times before the window shuts down
+    for i in range(5):
+        # .getmouse makes it to were the user can click with there mouse
+        p = win.getMouse()
+        # grabs the center of the previous circle
+        c = shape.getCenter()
+        # takes the previous x value and subtracts the new value
+        dx = p.getX() - c.getX()
+        # takes the y value and subtracts new value
+        dy = p.getY() - c.getY()
+        # finally moves the to the total new value caluclated were the user clicks
+        shape.move(dx, dy)
+    # after the computer runs everything it closes out the tab
+    win.close()
+
 
 main()
