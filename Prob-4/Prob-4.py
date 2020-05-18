6ffbcd3ef5573bd676ba7b4c69651bc19e6e5bc7
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-18 01:35:25 -0700

    stuck on 4 moving on to 5

--- Prob-4/Prob-4.py
+++ Prob-4/Prob-4.py
@@ -2,9 +2,38 @@
 #   Programming Assignment 6
 #       Prob-4.py
 
-# <YOUR NAME>
+# Eddy
+
+from graphics import *
+
 
 def main():
-    pass # remove this line
+    # makes the tab open 800,600 big
+    win = GraphWin("shapes", 800, 600)
+    # draws a rectangle tab
+
+    # get p1
+    p1 = win.getMouse()
+    # get p2
+    p2 = win.getMouse()
+    # draw rectangle
+    rect = Rectangle(Point(p1.getX(), p1.getY()), Point(p2.getX(), p2.getY()))
+    rect.draw(win)
+
+    # door
+    p3 = win.getMouse()
+    # get p1
+    # sidthdoor = (p2x - p1x) // 5
+    widthDoor = p2.getX() - p1.getY() // 5
+    # halfdoorwidth = widthdoor // 2
+    halfdoorwidth = widthDoor // 2
+
+    # pointfill = p1x - halfdoorwidth
+    Rec2 = Rectangle(Point(p3.getX() - halfdoorwidth, p1.getY()),
+                     Point(p3.getX() + halfdoorwidth, p1.getY()))
+    Rec2.draw(win)
+
+    input()
+
 
 main()
--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -2,10 +2,27 @@
 #   Programming Assignment 6
 #       Prob-5.py
 
-# <YOUR NAME>
+# eddy
+from graphics import *
+
 
 def main():
-    pass # remove this line 
+    win = GraphWin("shapes", 800, 600)
+    # rec xy and xy
+    rect = Rectangle(Point(150, 350), Point(600, 500))
+    rect.setFill("blue")
+    rect.draw(win)
+
+    # wheels
+    circ1 = Circle(Point(200, 500), 60)
+    circ1.setFill("black")
+    circ1.draw(win)
+
+    circ2 = Circle(Point(550, 500), 60)
+    circ2.setFill("black")
+    circ2.draw(win)
+
+    input()
 
 
 main()
